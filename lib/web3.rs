181ed00fcf4266fbe69642d06921a2976e3b2786
GiovanniTorrisi-ChainSecurity <giovanni.torrisi@chainsecurity.com>
2025-07-07 17:33:10 +0200

    Decrease max block range from 10000 to 5000

--- lib/web3.rs
+++ lib/web3.rs
@@ -29,7 +29,7 @@ use alloy_rpc_types_trace::parity::{
 use reth_trie::root;
 
 const NUM_STORAGE_QUERIES: u64 = 32;
-const LARGE_BLOCK_RANGE: u64 = 100000;
+const LARGE_BLOCK_RANGE: u64 = 5000;
 
 mod pathological_rpc_deserde {
     use serde::{de::Error, Deserialize};
