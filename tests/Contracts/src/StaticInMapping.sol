50d59dee8495d38a63cb462c290b5bb17de0502f
GiovanniTorrisi-ChainSecurity <giovanni.torrisi@chainsecurity.com>
2025-07-24 17:07:17 +0200

    Fix formatting

--- tests/Contracts/src/StaticInMapping.sol
+++ tests/Contracts/src/StaticInMapping.sol
@@ -13,14 +13,14 @@ contract StaticInMapping {
 
         static_in_mapping[address(this)] = 2 ** 256 - 1;
         static_in_mapping[msg.sender] = 5;
-        static_in_mapping2[2+3] = 45;
+        static_in_mapping2[2 + 3] = 45;
     }
 
     function assign() external {
         // compiler computes sha3 of key
         static_in_mapping[address(1)] = 16;
-        static_in_mapping2[1+15] = 42;
-        static_in_mapping2[1+16] = 81+2;
+        static_in_mapping2[1 + 15] = 42;
+        static_in_mapping2[1 + 16] = 81 + 2;
     }
 
     function dummy() external {}
